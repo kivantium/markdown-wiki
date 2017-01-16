3915a40cd4df92529509288f286ae49a419967f9
kivantium <kivantium@gmail.com>
2017-01-16 09:11:48 +0900

    Modify style

--- index.js
+++ index.js
@@ -9,31 +9,54 @@ app.engine('html', function (filePath, options, callback) {
   fs.readFile(filePath, 'utf8', function (err, content) {
     if (err) return callback(err)
     var rendered = content.toString().replace('#main#', options.main)
+                                     .replace('#title#', options.title);
     return callback(null, rendered)
   })
 });
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'html')
 
-var md = require('markdown-it')();
-var mk = require('markdown-it-katex');
-md.use(mk);
+var hljs = require('highlight.js')
+var md = require('markdown-it')({
+  html:         true,        // Enable HTML tags in source
+  breaks:       true,        // Convert '\n' in paragraphs into <br>
+  linkify:      true,        // Autoconvert URL-like text to links
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+    return ''; // use external default escaping
+  }
+});
+md.use(require('markdown-it-katex'));
+md.use(require('markdown-it-title'))
 
 app.get('/', function(req, res){
-    var result = md.render('#title');
+    var result = md.render('# Tips');
     res.render('template', { main: result })
+});
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendStatus(204);
 });
 
 app.get('/:entry', function(req, res){
     var filename = req.params.entry+'.md';
-    console.log('open %s', filename);
     fs.readFile(path.join(__dirname, filename), 'utf8', function (err, data) {
         if (err) {
             res.render('template', { main: '404 Not Found' });
-            return console.log(err);
+        } else {
+            const env = {};
+            var article = md.render(data, env);
+            res.render('template', { main: article, title: env.title });
         }
-        res.render('template', { main: md.render(data) });
     });
+});
+
+app.use(function(req, res){
+    res.render('template', { main: 'File does not exist' });
 });
 
 http.listen(3000, function(){
