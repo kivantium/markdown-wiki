680dd2f100190f1e6d18e860db40efd8edc7632e
kivantium <kivantium@gmail.com>
2017-02-01 10:23:40 +0900

    Add login

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ var app = express();
 var http = require('http').Server(app);
 var fs = require('fs')
 var path = require('path');
+var ejs = require('ejs');
+var session = require('express-session')
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
-app.engine('html', function (filePath, options, callback) {
-  fs.readFile(filePath, 'utf8', function (err, content) {
-    if (err) return callback(err)
-    var rendered = content.toString().replace('#main#', options.main)
-                                     .replace('#title#', options.title)
-                                     .replace('#link#', options.link);
-    return callback(null, rendered)
-  })
-});
+app.engine('ejs', ejs.renderFile);
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'html')
+app.set('view engine', 'ejs')
 
 var hljs = require('highlight.js')
 var md = require('markdown-it')({
@@ -37,8 +33,47 @@ md.use(require('markdown-it-katex'));
 md.use(require('markdown-it-title'));
 md.use(require('markdown-it-deflist'));
 
+
+var config = fs.readFileSync("config.json");
+var jsonConfig = JSON.parse(config);
+passport.use(new Strategy({
+    consumerKey: jsonConfig.consumerKey,
+    consumerSecret: jsonConfig.consumerSecret,
+    callbackURL: jsonConfig.callbackURL
+  },
+  function(token, tokenSecret, profile, cb) {
+    return cb(null, profile);
+}));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+app.use(session({
+    secret: 'keyboard alice',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res){
-    res.redirect('/index');
+    var username = 'guest';
+    if(req.user) {
+        username = req.user.username;
+    }
+    var filename = 'index';
+    fs.readFile(path.join(__dirname, 'md/'+filename+'.md'), 'utf8', function (err, data) {
+        const env = {};
+        var article = md.render(data, env);
+        res.render('template.ejs', { title: env.title,
+                                     main: article,
+                                     link: filename,
+                                     username: username});
+    });
 });
 
 app.get('/favicon.ico', function(req, res) {
@@ -46,28 +81,52 @@ app.get('/favicon.ico', function(req, res) {
 });
 
 app.get('/:entry', function(req, res){
+    var username = 'guest';
+    if(req.user) {
+        username = req.user.username;
+    }
     var filename = req.params.entry;
     fs.readFile(path.join(__dirname, 'md/'+filename+'.md'), 'utf8', function (err, data) {
         if (err) {
-            res.render('template', { title: 'Not Found', main: 'Not Found.', link: filename});
+            res.render('template.ejs', { title: 'Page Not Found', 
+                                         main: 'Page Not Found.', 
+                                         link: filename,
+                                         username: username});
         } else {
             const env = {};
             var article = md.render(data, env);
-            res.render('template', { title: env.title, main: article, link: filename});
+            res.render('template.ejs', { title: env.title,
+                                         main: article,
+                                         link: filename,
+                                         username: username});
         }
     });
 });
 
 app.get('/edit/:entry', function(req, res){
-    var filename = req.params.entry;
-    fs.readFile(path.join(__dirname, 'md/'+filename+'.md'), 'utf8', function (err, data) {
-        if (err) {
-            res.render('edit', { title: filename, main: '', link: filename});
-        } else {
-            res.render('edit', { title: filename, main: data, link: filename});
-        }
-    });
+    if(req.user && req.user.username == 'kivantium') {
+        var filename = req.params.entry;
+        fs.readFile(path.join(__dirname, 'md/'+filename+'.md'), 'utf8', function (err, data) {
+            if (err) {
+                res.render('edit', { title: filename, main: '', link: filename});
+            } else {
+                res.render('edit', { title: filename, main: data, link: filename});
+            }
+        });
+    } else {
+        res.redirect('/login/twitter');
+    }   
 });
+
+app.get('/login/twitter',
+  passport.authenticate('twitter')
+);
+
+app.get('/login/twitter/callback', 
+  passport.authenticate('twitter', { successRedirect:'/',
+                                     failureRedirect: '/login/twitter',
+                                     failureFlash: true })
+);
 
 app.post('/save', function(req, res) {
     var input = JSON.parse(JSON.stringify(req.body));
